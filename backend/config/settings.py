0c6f856e67d29e9875855df921c62596fe7d8dd7
Amirul-url <muhdamirulaqmal@gmail.com>
2026-06-24 11:12:50 +0800

    Use blob previews for attachments

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -161,7 +161,6 @@ AUTH_PASSWORD_VALIDATORS = []
 # DRF + JWT
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'config.authentication.QueryParamJWTAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_THROTTLE_CLASSES': (
