6488605e78aaf1f2f2cce1e0be8105c61176b181
Amirul-url <muhdamirulaqmal@gmail.com>
2026-07-27 14:45:53 +0800

    Disable standalone payment queue

--- frontend/src/pages/admin/payment/PaymentPage.jsx
+++ frontend/src/pages/admin/payment/PaymentPage.jsx
@@ -1,6 +1,14 @@
+import { Navigate, useLocation } from "react-router-dom";
 import ProcessWorkspace from "../ProcessWorkspace";
 
 function PaymentPage() {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  if (!searchParams.get("id")) {
+    return <Navigate to="/dashboard/admin?view=personal" replace />;
+  }
+
   return <ProcessWorkspace type="payment" />;
 }
 
